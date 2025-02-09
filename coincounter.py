8c8fb8e01078aca73dfcd442c631c2cebde9a94b
UmiTheForce <aratliff1112@gmail.com>
2025-02-09 17:08:11 -0500

    quarter function added

--- coincounter.py
+++
@@ -1,8 +0,0 @@
-#functions to calculate dollar totals of each coin
-def quarters():
-    """function to calculate dollar totals of quarters"""
-    qTotal = (quarter * 25) / 100
-    return float(qTotal)
-
-quarter = int(input("How many quarters? "))
-print(quarters())    
---
+++ counter.py
@@ -0,0 +1,5 @@
+#functions to calculate dollar totals of coins
+def quarters():
+    """function calculates dollar total of quarters"""
+    qTotal = (quarter * 25) / 100
+    return float(qTotal)
