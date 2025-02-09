626be5619e2a2c7d01f8ce450ba6fd85d7963b38
UmiTheForce <aratliff1112@gmail.com>
2025-02-09 18:05:13 -0500

    added functions for dimes, nickles, pennies

--- counter.py
+++ counter.py
@@ -3,3 +3,15 @@ def quarters():
     """function calculates dollar total of quarters"""
     qTotal = (quarter * 25) / 100
     return float(qTotal)
+def dimes():
+    """function calculates dollar total of dimes"""
+    dTotal = (dime * 10) / 100
+    return float(dTotal)
+def nickles():
+    """function calculates dollar total of nickles"""
+    nTotal = (nickle * 5) / 100
+    return float(nTotal)
+def pennies():
+    """function calculates dollar total of pennies"""
+    pTotal = penny / 100
+    return float(pTotal)
